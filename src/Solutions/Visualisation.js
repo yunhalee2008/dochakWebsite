d177f5fc5d31014b610a844b8e8bed8a74d4ad12
InhiKim <inhikim2775@gmail.com>
2025-09-01 20:31:02 +0900

    visualization

--- src/Solutions/Visualisation.js
+++ src/Solutions/Visualisation.js
@@ -59,7 +59,7 @@ export default function Visualisation() {
               <div className="video-wrapper">
                 <div className="video-container">
                   <iframe
-                    src="https://www.youtube.com/embed/uTBSurLXXqk?si=rOd5nQ5FnZ7SmEuq"
+                    src="https://www.youtube.com/embed/bE57_-qdaq0?si=64ShTTK665vgF-wM"
                     title={t('solutions.visualisationSolution.overview.demo')}
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
